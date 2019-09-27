eabbf0c3ec63b108f27bbf3aefebbdca7cb6e7e6
rrbb014 <gutssoul1@gmail.com>
2019-09-27 19:31:59 +0900

    ADDED: text abstraction and matched status

--- gauss_keeker/common.py
+++ gauss_keeker/common.py
@@ -1,4 +1,5 @@
 import re
+from copy import copy
 
 
 class RegexRepository:
@@ -11,6 +12,14 @@ class RegexRepository:
             values = list(re.search(regex, string).groups())
             keys = re.findall(r'{(.+?)}', pattern)
             _dict = dict(zip(keys, values))
+
+            abstract_string = copy(string)
+            for k, v in _dict.items():
+                abstract_string = abstract_string.replace(v, '{'+k+'}')
+            
+            _dict['abstract'] = abstract_string
+            _dict['matched'] = bool(re.findall(pattern, abstract_string)) 
+
             return _dict
         except AttributeError:
             # TODO: To handling multi-line log, it may be deleted
