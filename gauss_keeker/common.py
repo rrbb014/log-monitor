c78cd69d173a832a235a2b53243dac2de260f9e2
rrbb014 <gutssoul1@gmail.com>
2019-10-07 20:24:40 +0900

    CHANGED: Same substring replaced twice. force only 1 time replacement

--- gauss_keeker/common.py
+++ gauss_keeker/common.py
@@ -10,6 +10,7 @@ class RegexRepository:
 
     def check_matched(self, string, pattern):
         regex = re.sub(r'{(.+?)}', r'(?P<_\1>.+)', pattern)
+
         searched = re.search(regex, string)
         if searched:
             values = list(searched.groups())
@@ -18,30 +19,23 @@ class RegexRepository:
         keys = re.findall(r'{(.+?)}', pattern)
         abstract_string = copy(string)
         for k, v in zip(keys, values):
-            abstract_string = abstract_string.replace(v, '{'+k+'}')
+            abstract_string = abstract_string.replace(v, '{'+k+'}', 1)
 
         matched = bool(re.findall(pattern, abstract_string)) 
         return matched
 
-    def string_to_dict(self, string, pattern, match_check=False):
+    def string_to_dict(self, string, pattern):
         # Thanks to url:
         # https://stackoverflow.com/questions/11844986/convert-or-unformat-a-string-to-variables-like-format-but-in-reverse-in-p/11849360#11849360
         try:
+            # TODO: Cannot parse multi-line error log. Because of newline(\n).
             regex = re.sub(r'{(.+?)}', r'(?P<_\1>.+)', pattern)
             values = list(re.search(regex, string).groups())
             keys = re.findall(r'{(.+?)}', pattern)
             _dict = dict(zip(keys, values))
 
-            if match_check:
-                abstract_string = copy(string)
-                for k, v in _dict.items():
-                    abstract_string = abstract_string.replace(v, '{'+k+'}')
-                
-                _dict['abstract'] = abstract_string
-                _dict['matched'] = bool(re.findall(pattern, abstract_string)) 
-
             return _dict
         except AttributeError:
-            # TODO: To handling multi-line log, it may be deleted
-            _dict = dict(message=string)
-            return _dict
+            self.logger.exception('AttributeError occured. message: %s, pattern: %s' % (string, pattern))
+        except:
+            self.logger.exception("Unexpected error occured. Maybe cannot search. message: %s, pattern: " % (string, pattern))
